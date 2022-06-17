e4e74a7bec63838d07eda35ff6b25007b7b29f60
仕旻 張 <michael19950629@gmail.com>
2022-06-17 16:12:54 +0800

    0617教室

--- CSharp_HomeWork/Homework02_a.cs
+++ CSharp_HomeWork/Homework02_a.cs
@@ -25,12 +25,22 @@ namespace CSharp_HomeWork
 
         private void PMTBut_Click(object sender, EventArgs e)
         {
-            
-            
-            string TargetST = TargetLB01.Text;
-            int Target = Convert.ToInt32(TargetST);
-            
-        }
+
+
+            //string TargetST = TargetLB01.Text;
+            //int Target = Convert.ToInt32(TargetST);
+            int Target = Convert.ToInt32(TargetTB01.Text);
+            int TimeLimit = Convert.ToInt32(TimelimitTB01.Text);
+            double Rate = Convert.ToInt32(RateTB01.Text);
+            int DP = Convert.ToInt32(DPTB.Text);
+            double moonrate = 1 + (Rate / 12.0);
+
+            //公式錯了
+            int yearrate = (int)(Math.Pow(moonrate,12)*(Rate/12)/(Math.Pow(moonrate, 12) - 1));
+            int moonpay = (Target - DP) * yearrate;
+
+            MessageBox.Show(Convert.ToString(moonpay));
+}
 
         /*private void FortestBut_Click(object sender, EventArgs e)
         {
