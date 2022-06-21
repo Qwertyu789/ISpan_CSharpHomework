437853bd8a0f09c836e330983f888f0ad25dfebc
仕旻 張 <michael19950629@gmail.com>
2022-06-21 18:34:38 +0800

    0621 CLASS

---
+++ CSharp_HomeWork/Homework05.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_HomeWork
+{
+    public partial class Homework05 : Form
+    {
+        public Homework05()
+        {
+            InitializeComponent();
+        }
+    }
+}
